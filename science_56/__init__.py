1132ee77b7aeb1dc8f50b85292a483bc2fb83be6
Lathif Ramadhan <168287240+LatiefDataVisionary@users.noreply.github.com>
2025-01-25 20:57:14 +0700

    Add files via upload

---
+++ science_56/__init__.py
@@ -0,0 +1,20 @@
+# print("Latief ganteng!!!")
+
+# kegantengan = input(print("Masukkan level kegantengan 1-10 : "))
+
+# print(f"Kegantengan Latief adalah: " + {kegantengan})
+
+
+from . import matematika 
+from . import fisika  
+
+
+## Atau biar gk ribet
+## Sebenarnya dibawah ini tdk disarankan 
+# __all__ = [
+#     "matematika",
+#     "fisika"
+# ]
+
+
+
