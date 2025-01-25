439445e1c4da9913604e91b59fa8be09f4807c27
Lathif Ramadhan <168287240+LatiefDataVisionary@users.noreply.github.com>
2025-01-25 20:56:30 +0700

    Add files via upload

---
+++ py_tasks/Latsol Aritmatika.py
@@ -0,0 +1,137 @@
+print("\n" + "=" * 6 + "Latsol Aritmatika" + "=" * 6)
+
+""""
+    Ubahlah nilai dari variabel a dan b menjadi 8 dan 3. 
+    Kemudian, hitunglah hasil dari operasi berikut: a + b ** 2 - a // b + b % a
+"""
+print("====Soal No. 1====")
+a = 8
+b = 3
+c = a + b ** 2 - a // b + b % a
+print(c)
+
+
+""""
+    Buatlah sebuah program yang meminta pengguna untuk memasukkan dua angka, 
+    lalu mencetak hasil dari operasi 
+    penjumlahan, pengurangan, perkalian, pembagian, 
+    eksponen, modullus, dan floor division antara kedua angka tersebut.
+"""
+
+print("\n====Soal No. 2====")
+# a = float(input("Masukkan angka a : "))
+# b = float(input("Masukkan angka b : "))
+
+# penjumlahan = a + b
+# pengurangan = a - b
+# perkalian = a * b
+# pembagian = a / b
+# eksponen = a ** b
+# modullus = a % b
+# floor_division = a // b
+
+# print("Hasil penjumlahan = ", penjumlahan)
+# print('Atau ', a,'+', b,'=', penjumlahan)
+
+# print("Hasil pengurangan = ", pengurangan)
+# print('Atau ', a,'-', b,'=', pengurangan)
+
+# print("Hasil perkalian = ", perkalian)
+# print('Atau ', a,'*', b,'=', perkalian)
+
+# print("Hasil pembagian = ", pembagian)
+# print('Atau ', a,'/', b,'=', pembagian)
+
+# print("Hasil eksponen = ", eksponen)
+# print('Atau ', a,'**', b,'=', eksponen)
+
+# print("Hasil modullus = ", modullus)
+# print('Atau ', a,'%', b,'=', modullus)
+
+# print("Hasil floor division = ", floor_division)
+# print('Atau ', a,'//', b,'=', floor_division)
+
+
+print("\n====Soal No. 3====")
+""""
+    Buatlah sebuah program yang meminta pengguna 
+    untuk memasukkan sebuah bilangan bulat positif, 
+    lalu mencetak apakah bilangan tersebut ganjil atau genap. 
+    Gunakanlah operasi modullus untuk menentukan hal ini.
+"""
+
+a = int(input("Masukkan bil. bulat positif : "))
+b = a % 2
+if a == 1:
+    print("Ini merupakan bilangan Ganjil")
+else:
+    print("Ini merupakan bilangan Genap")
+
+
+print("\n====Soal No. 4====")
+""""
+    Buatlah sebuah program yang meminta pengguna 
+    untuk memasukkan sebuah bilangan bulat positif, 
+    lalu mencetak faktorial dari bilangan tersebut. 
+    Faktorial dari n adalah hasil dari perkalian 
+    semua bilangan bulat positif yang lebih kecil atau sama dengan n. 
+    Contoh: faktorial dari 5 adalah 5 x 4 x 3 x 2 x 1 = 120.
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("\n")
